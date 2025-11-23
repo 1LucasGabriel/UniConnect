22bc3e333d24cc57a09889394ed82cf89e4ac20e
Lucas Gabriel <lucaas.gabriel1804@gmail.com>
2025-11-23 19:01:27 -0300

    CRUD de usuário

--- src/UniConnect.Api/Program.cs
+++ src/UniConnect.Api/Program.cs
@@ -1,49 +1,36 @@
 using Microsoft.EntityFrameworkCore;
+using UniConnect.Application.Services;
+using UniConnect.Application.Services.Interfaces;
+using UniConnect.Domain.Interfaces;
 using UniConnect.Infrastructure.Context;
+using UniConnect.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
+builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+builder.Services.AddScoped<IUsuarioService, UsuarioService>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
---
+++ src/UniConnect.Domain/Interfaces/IUsuarioRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniConnect.Domain.Entities;
+
+namespace UniConnect.Domain.Interfaces
+{
+    public interface IUsuarioRepository
+    {
+        Task<List<UsuarioEntity>> Buscar();
+        Task<UsuarioEntity> BuscarPorId(int id);
+        Task Adicionar(UsuarioEntity usuario);
+        Task Atualizar(int id, UsuarioEntity usuario);
+        Task Deletar(int id);
+    }
+}
